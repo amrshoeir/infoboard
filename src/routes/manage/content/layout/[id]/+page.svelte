c732c5444b9e0aba6ad271dc83d9df36b89032ff
Amr Sheir <a.mohshoeir@gmail.com>
2023-07-02 18:23:48 +0200

    Final

---
+++ src/routes/manage/content/layout/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  export let data
+  const layout = data.layout
+</script>
+<form method="POST" action="/manage/content?/editLayout" use:enhance>
+  <label>Description: <input type="text" name="description"  bind:value={layout.layout_data.description} ></label>
+  <label>Duration (default) : <input type="number" name="duration" required bind:value={layout.duration}></label>
+  <input type="hidden" name="id" bind:value={layout.id}>
+  <div class="container">
+    <button class="primary edit">Edit</button>
+  </div>
+</form>
